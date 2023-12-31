39de5ed8cda016a1cc1f632924390b074e9a634c
Kyle <kdleonard@icloud.com>
2023-12-31 17:05:34 -0600

    Update to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,26 +1,15 @@
 <footer
 	class="py-20 sm:py-32 border-t border-solid border-primary-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-	<p
-		class="px-4 py-2 font-medium relative overflow-hidden px-5 py-2 group rounded-full bg-primary-500"
-	>
-		Connect with me &darr;
+	<p class="px-4 py-2 font-medium relative overflow-hidden px-5 py-2 group bg-white text-black">
+		Connect with me! &darr;
 	</p>
 
-	<div class="flex flex-col gap-4 items-center justify-center">
-		<!-- <div
-            class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
-        >
-            <a>
-                <i class="fa-brands fa-linkedin-in" />
-            </a>
-            <a>
-                <i class="fa-brands fa-github" />
-            </a>
-        </div>
-        <p class="mx-auto text-sm opacity-70">or</p>
-        <p class="">Hugh_jass@gmail.com</p> -->
-		<p><b class="pr-2">Email</b> contact@digitaldopamine.dev</p>
+	<div class="flex flex-col gap-4 items-center justify-center sm:gap-6 mx-auto">
+		<a href="mailto:contact@digitaldopamine.dev"
+			><b class="pr-2">Email</b>
+			<span class="text-primary-400 hover:text-">contact@digitaldopamine.dev</span></a
+		>
 		<p>
 			<b class="pr-2">GitHub</b>
 			<a href="https://github.com/kdleonard93" target="_blank" class="text-primary-400"
@@ -33,7 +22,23 @@
 		</p>
 		<p>
 			<b class="pr-2">LinkedIn</b>
-			<a href="https://www.linkedin.com/in/kyle-leonard93/" target="_blank" class="text-primary-400"
+			<a
+				href="https://www.linkedin.com/in/kyle-leonard93/"
+				target="_blank"
+				class="text-primary-400 hover:text-white"
+				>Kyle Leonard<sup class=""
+					><span class="text-xs scale-75 pl-0.5"
+						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span
+					></sup
+				></a
+			>
+		</p>
+		<p>
+			<b class="pr-2">Discord</b>
+			<a
+				href="https://www.linkedin.com/in/kyle-leonard93/"
+				target="_blank"
+				class="text-primary-400 hover:text-white"
 				>Kyle Leonard<sup class=""
 					><span class="text-xs scale-75 pl-0.5"
 						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span
